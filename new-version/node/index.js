a20a1b80bbfa89e8275e3dc221af9c94d7910f96
mikemalkhasyan <mkrtich.malkhasyan1996@gmail.com>
2022-03-19 15:38:04 +0400

    - implement Node class && toLabel() method

--- new-version/node/index.js
+++ new-version/node/index.js
@@ -1,1 +1,33 @@
-// TODO Implement Node class
+const NodeBase = require('./base');
+
+class Node extends NodeBase {
+    constructor(
+        operator = null,
+        value,
+        left,
+        right
+    ) {
+        super()
+        this.operator = operator;
+        this.value = value;
+        this.left = left;
+        this.right = right;
+    }
+
+    // In old version this method called as toString() that is not correct ->
+    // because it's javascript objects core method name, that might cause serious bugs going forward
+    // So, I renamed it to be called as 'toLabel'
+    toLabel() {
+        if (
+            null !== this.operator &&
+            this.supportedOperatorLabels.has(this.operator)
+        ) {
+            const operatorLabelFunc = this.supportedOperatorLabels.get(this.operator);
+            return operatorLabelFunc(this.left.toLabel(), this.right.toLabel());
+        }
+
+        return this.value;
+    }
+}
+
+module.exports = Node;
